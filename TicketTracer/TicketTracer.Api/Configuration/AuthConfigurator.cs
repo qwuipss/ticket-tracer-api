20aebf2d12a2a9eb26f742b20a7a946a087534d8
ivan <qwuipss@icloud.com>
2025-05-22 16:12:01 +0500

    fix expiration

--- TicketTracer/TicketTracer.Api/Configuration/AuthConfigurator.cs
+++ TicketTracer/TicketTracer.Api/Configuration/AuthConfigurator.cs
@@ -14,7 +14,7 @@ internal static class AuthConfigurator
                     options.Cookie.HttpOnly = true;
                     options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
                     options.Cookie.SameSite = SameSiteMode.Lax;
-                    options.ExpireTimeSpan = TimeSpan.FromDays(7);
+                    options.Cookie.Expiration = TimeSpan.FromDays(7);
                     options.SlidingExpiration = true;
                     options.Events = new CookieAuthenticationEvents
                     {
